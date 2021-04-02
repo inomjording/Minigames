90d3dd2dd0aad0972b9d21193ccd24bbc96a5775
Adriel <ddeellaa@live.se>
2021-04-03 01:00:52 +0200

    added SumHand in Hand

--- Blackjack/Hand.cs
+++ Blackjack/Hand.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Blackjack
 {
-    public class Hand
+    public class Hand<T>
     {
         public ArrayList Cards = new ArrayList();
 
@@ -12,14 +12,62 @@ namespace Blackjack
             
         }
 
-        public void AddCard(int card)
+        public void AddCard(T card)
         {
-            this.Cards.Add(card);
+            if (card as string == "A")
+            {
+                this.Cards.Add(card);
+            }
+
+            else
+            {
+                this.Cards.Insert(0, card);
+            }
+            
         }
 
         public int SumHand()
         {
-            return 0;
+            var sum = 0;
+            var aceCount = 0;
+            foreach (var card in this.Cards)
+            {
+                
+                switch (card)
+                {
+                    case "K":
+                        sum += 10;
+                        break;
+
+                    case "A":
+                        aceCount++;
+
+                        break;
+                        
+                    default:
+                        sum += (int) card;
+                        break;
+                }
+
+                for (var i = 0; i < aceCount; i++)
+                {
+                    if (sum + 11 > 21)
+                    {
+                        sum += 1;
+                        if (sum > 21 && i+1 < aceCount && i > 0)
+                        {
+                            sum -= 10;
+                        }
+                    }
+    
+                    else
+                    {
+                        sum += 11; 
+                    }
+                }
+                
+            }
+            return sum;
         }
     }
 }
