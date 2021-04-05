8303df167208cb3ef825cceb196e7c2974e6cf22
Adriel <ddeellaa@live.se>
2021-04-05 19:02:40 +0200

    Added DrawCard in Decks

--- Blackjack/Deck.cs
+++ Blackjack/Deck.cs
@@ -1,27 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace Blackjack
 {
-    public class Deck
+    public class Deck<T>
     {
-        public ArrayList Cards = new ArrayList();
+        private ArrayList _cards;
+        private static Random _rnd = new Random();
         
         public Deck()
         {
+            this._cards = BuildDeck();
         }
 
-        private void BuildDeck()
+        private static ArrayList BuildDeck()
         {
+            var cards = new ArrayList();
             for (var i = 2; i < 11; i++)
             {
                 var temp = ArrayList.Repeat(i, 4);
-                this.Cards.AddRange(temp);
+                cards.AddRange(temp);
             }
 
             var face = ArrayList.Repeat("K", 12);
             var ace = ArrayList.Repeat("A", 4);
-            this.Cards.AddRange(face);
-            this.Cards.AddRange(ace);
+            cards.AddRange(face);
+            cards.AddRange(ace);
+            return cards;
+        }
+
+        public T DrawCard()
+        {
+            var r = _rnd.Next(0, this._cards.Count);
+            var card = (T) this._cards[r];
+            this._cards.RemoveAt(r);
+            return card;
         }
     }
 }
